3d6528ee85bc5b94472ebf170d914c3d37aed0a4
Madhumitha1374 <46565222+Madhumitha1374@users.noreply.github.com>
2024-12-03 19:44:20 +0530

    Add files via upload

---
+++ WpfTraining/Wpf_Training1/Wpf_Training1/Cs_Code/First_Cs.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf_Training1.Cs_Code
+{
+    /// <summary>
+    /// Interaction logic for First_Cs.xaml
+    /// </summary>
+    public partial class First_Cs : Window
+    {
+        public First_Cs()
+        {
+            InitializeComponent();
+        }
+
+        private void First_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hi this is first Button");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hi this is second Button");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            First_Cs_window.Name = "Hi Madhumitha!";
+        }
+    }
+}
